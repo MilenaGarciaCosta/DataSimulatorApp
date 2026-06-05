551501d4ee7780aa6db12639a54729fdd7ac1b3f
MilenaGarciaCosta <milenagarcia.dev@gmail.com>
2026-06-04 21:00:12 -0300

    Add project files.

---
+++ DataSimulatorApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DataSimulatorApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
